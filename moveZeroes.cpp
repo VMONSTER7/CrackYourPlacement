78e03d34e29b654bf9860a59c67b27a8925b0165
VASTVIKTA NISHAD <96430679+VMONSTER7@users.noreply.github.com>
2024-07-15 18:43:07 +0530

    move 0 to the last of the array

---
+++ moveZeroes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = 0, j = 0;
+
+        while(j<nums.size())
+        {
+            if(nums[i] == 0 && nums[j]!=0)
+            {
+                swap(nums[i],nums[j]);
+               
+            }
+           if(nums[i]!=0)
+            {
+                i++;
+            }
+
+                j++;
+        }
+    }
+};
